d1776ef8105ab70b1501d11ca83d59c5da53c3d2
Rafael F Beraldo <rafael.pimpa@gmail.com>
2025-09-27 11:10:51 -0300

    feat: add mysql single row map scan

---
+++ internal/scan/scanner_test.go
@@ -0,0 +1,226 @@
+package scan
+
+import (
+	"cmp"
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/rfberaldo/sqlz/internal/testutil"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func connect(driverName, dataSourceName string) (*sql.DB, error) {
+	db, err := sql.Open(driverName, dataSourceName)
+	if err != nil {
+		log.Printf("error connecting to %v: %v", driverName, err)
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Printf("error pinging to %v: %v", driverName, err)
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func TestScan(t *testing.T) {
+	dsn := cmp.Or(os.Getenv("MYSQL_DSN"), testutil.MYSQL_DSN)
+	db, err := connect("mysql", dsn)
+	require.NoError(t, err)
+
+	rows, err := db.Query("SELECT * FROM user")
+	require.NoError(t, err)
+
+	var id, age int
+	var username string
+	var value float64
+
+	defer rows.Close()
+
+	fmt.Println(rows.Columns())
+	ct, err := rows.ColumnTypes()
+	require.NoError(t, err)
+
+	for _, v := range ct {
+		fmt.Printf("%#v\n", *v)
+	}
+
+	for rows.Next() {
+		err = rows.Scan(&id, &username, &age, &value)
+		require.NoError(t, err)
+	}
+
+	err = rows.Err()
+	require.NoError(t, err)
+
+	fmt.Println(id, username, age, value)
+}
+
+func TestScanner_ScanMap(t *testing.T) {
+	dsn := cmp.Or(os.Getenv("MYSQL_DSN"), testutil.MYSQL_DSN)
+	db, err := connect("mysql", dsn)
+	require.NoError(t, err)
+
+	t.Run("should work on allocated map", func(t *testing.T) {
+		rows, err := db.Query("SELECT * FROM user LIMIT 1")
+		require.NoError(t, err)
+		scanner := &Scanner{queryRow: true, rows: rows}
+		m := make(map[string]any)
+		err = scanner.Scan(m)
+		require.NoError(t, err)
+		assert.Len(t, m, 4)
+	})
+
+	t.Run("should work on allocated map pointer", func(t *testing.T) {
+		rows, err := db.Query("SELECT * FROM user LIMIT 1")
+		require.NoError(t, err)
+		scanner := &Scanner{queryRow: true, rows: rows}
+		m := make(map[string]any)
+		err = scanner.Scan(&m)
+		require.NoError(t, err)
+		assert.Len(t, m, 4)
+	})
+
+	t.Run("should work on non allocated map pointer", func(t *testing.T) {
+		rows, err := db.Query("SELECT * FROM user LIMIT 1")
+		require.NoError(t, err)
+		scanner := &Scanner{queryRow: true, rows: rows}
+		var m map[string]any
+		err = scanner.Scan(&m)
+		require.NoError(t, err)
+		assert.Len(t, m, 4)
+	})
+
+	t.Run("should work map slice", func(t *testing.T) {
+		rows, err := db.Query("SELECT * FROM user")
+		require.NoError(t, err)
+		scanner := &Scanner{rows: rows}
+		var m []map[string]any
+		err = scanner.Scan(&m)
+		require.NoError(t, err)
+		assert.Len(t, m, 3)
+
+		for k, v := range m {
+			fmt.Println(k, v)
+		}
+	})
+}
+
+func TestScanner_ScanArgs(t *testing.T) {
+	scanner := &Scanner{rows: &MockRows{}}
+
+	t.Run("should not fail on nil map", func(t *testing.T) {
+		var m map[string]any
+		err := scanner.Scan(m)
+		require.NoError(t, err)
+	})
+
+	t.Run("should not fail on nil map pointer", func(t *testing.T) {
+		var m map[string]any
+		err := scanner.Scan(&m)
+		require.NoError(t, err)
+	})
+
+	t.Run("should not fail on allocated map", func(t *testing.T) {
+		m := make(map[string]any)
+		err := scanner.Scan(&m)
+		require.NoError(t, err)
+	})
+
+	t.Run("should not fail on primitive pointer", func(t *testing.T) {
+		var m string
+		err := scanner.Scan(&m)
+		require.NoError(t, err)
+	})
+
+	t.Run("should not fail on nil pointer", func(t *testing.T) {
+		var m *string
+		err := scanner.Scan(&m)
+		require.NoError(t, err)
+	})
+
+	t.Run("should not fail on interface pointer", func(t *testing.T) {
+		var m any
+		err := scanner.Scan(&m)
+		require.NoError(t, err)
+	})
+
+	t.Run("should fail if not a primitive pointer", func(t *testing.T) {
+		var m string
+		err := scanner.Scan(m)
+		require.Error(t, err)
+	})
+
+	t.Run("should not fail if not a interface pointer", func(t *testing.T) {
+		var m any
+		err := scanner.Scan(m)
+		require.NoError(t, err)
+	})
+}
+
+type MockRows struct {
+	CloseFunc         func() error
+	ColumnTypesFunc   func() ([]*sql.ColumnType, error)
+	ColumnsFunc       func() ([]string, error)
+	ErrFunc           func() error
+	NextFunc          func() bool
+	NextResultSetFunc func() bool
+	ScanFunc          func(dest ...any) error
+}
+
+func (m *MockRows) Close() error {
+	if m.CloseFunc == nil {
+		return nil
+	}
+	return m.CloseFunc()
+}
+
+func (m *MockRows) ColumnTypes() ([]*sql.ColumnType, error) {
+	if m.ColumnTypesFunc == nil {
+		return nil, nil
+	}
+	return m.ColumnTypesFunc()
+}
+
+func (m *MockRows) Columns() ([]string, error) {
+	if m.ColumnsFunc == nil {
+		return nil, nil
+	}
+	return m.ColumnsFunc()
+}
+
+func (m *MockRows) Err() error {
+	if m.ErrFunc == nil {
+		return nil
+	}
+	return m.ErrFunc()
+}
+
+func (m *MockRows) Next() bool {
+	if m.NextFunc == nil {
+		return false
+	}
+	return m.NextFunc()
+}
+
+func (m *MockRows) NextResultSet() bool {
+	if m.NextResultSetFunc == nil {
+		return false
+	}
+	return m.NextResultSetFunc()
+}
+
+func (m *MockRows) Scan(dest ...any) error {
+	if m.ScanFunc == nil {
+		return nil
+	}
+	return m.ScanFunc(dest...)
+}
